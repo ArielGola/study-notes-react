db978ad70bc3908cb29ad6e914fd6a61a02e1006
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-01-07 10:20:04 -0300

    Improve switch because it had a mistake

--- src/components/CanvasComp/CanvasComp.jsx
+++ src/components/CanvasComp/CanvasComp.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 
-var propsValues;
-
 function CanvasComp(props) {
 
     const execFunc = () => { 
@@ -9,19 +7,19 @@ function CanvasComp(props) {
         return func();
     };
 
-
     useEffect(() => {
 
         var x = 0;
         var y = 0;
+        var x2 = 0;
+        var y2 = 0;
         var drawing = false;
+        
+        var propsValues;
 
         var canvas = document.getElementById('Canvas');
         var context = canvas.getContext('2d');
         var rect = canvas.getBoundingClientRect();
-
-        propsValues = execFunc();
-        //console.log(propsValues);
 
         canvas.addEventListener('mousedown', (e) => {
             x = e.clientX - rect.left;
@@ -30,25 +28,30 @@ function CanvasComp(props) {
         })
 
         canvas.addEventListener('mousemove', (e) => {
-            let x2 = e.clientX - rect.left;
-            let y2 = e.clientY - rect.top;
+            propsValues = execFunc();
+            x2 = e.clientX - rect.left;
+            y2 = e.clientY - rect.top;
 
             if (drawing === true && propsValues.selectedTool.pencil) {
-                onDrawing(x, y, x2, y2, context);
+                onDrawing(x, y, x2, y2, context, propsValues);
                 x = e.clientX - rect.left;
                 y = e.clientY - rect.top;
+                x2 = 0;
+                y2 = 0;
             }
         })
 
         canvas.addEventListener('mouseup', (e) => {
-
-            let x2 = e.clientX - rect.left;
-            let y2 = e.clientY - rect.top;
+            propsValues = execFunc();
+            x2 = e.clientX - rect.left;
+            y2 = e.clientY - rect.top;
 
             if (drawing === true) {
-                onDrawing(x, y, x2, y2, context);
+                onDrawing(x, y, x2, y2, context, propsValues);
                 x = 0;
                 y = 0;
+                x2 = 0;
+                y2 = 0;
                 drawing = false;
             }
         })
@@ -59,8 +62,7 @@ function CanvasComp(props) {
         
     }, [])
 
-    function onDrawing(x1, y1, x2, y2, context) {
-        propsValues = execFunc();
+    function onDrawing(x1, y1, x2, y2, context, propsValues) {
         if (propsValues.selectedTool.pencil) asPencil(x1, y1, x2, y2, context, propsValues);
         if (propsValues.selectedTool.line) asLine(x1, y1, x2, y2, context, propsValues);
     };
