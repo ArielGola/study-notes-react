270d5d970be2229245a1180248ceab0dd1f9e049
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-01-10 10:26:27 -0300

    Add functionallity of curve with the form quadraticCurve

--- src/components/CanvasComp/CanvasComp.jsx
+++ src/components/CanvasComp/CanvasComp.jsx
@@ -17,6 +17,9 @@ function CanvasComp(props) {
         
         var propsValues;
 
+        var curveX = [];
+        var curveY = [];
+
         var canvas = document.getElementById('Canvas');
         var context = canvas.getContext('2d');
         var rect = canvas.getBoundingClientRect();
@@ -40,22 +43,37 @@ function CanvasComp(props) {
                     x2 = 0;
                     y2 = 0;
                 }
+                if (propsValues.selectedTool.curve) {
+                    curveX.push(x2);
+                    curveY.push(y2);
+                }
             }
         })
 
         canvas.addEventListener('mouseup', (e) => {
             propsValues = execFunc();
+            
             x2 = e.clientX - rect.left;
             y2 = e.clientY - rect.top;
+            
+            let itemX = Number((curveX.length / 2).toFixed());
+            let itemY = Number((curveY.length / 2).toFixed());
+
+            let curvePointX = curveX[itemX];
+            let curvePointY = curveY[itemY];
+
 
             if (drawing === true) {
-                onDrawing(x, y, x2, y2, context, propsValues);
+                onDrawing(x, y, x2, y2, context, propsValues, curvePointX, curvePointY);
                 x = 0;
                 y = 0;
                 x2 = 0;
                 y2 = 0;
                 drawing = false;
             }
+
+            curveX = [];
+            curveY = [];
         })
 
         canvas.addEventListener('mouseout', () => {
@@ -64,10 +82,11 @@ function CanvasComp(props) {
         
     }, [])
 
-    function onDrawing(x1, y1, x2, y2, context, propsValues) {
+    function onDrawing(x1, y1, x2, y2, context, propsValues, curvePointX, curvePointY) {
         if (propsValues.selectedTool.pencil) asPencil(x1, y1, x2, y2, context, propsValues);
         if (propsValues.selectedTool.line) asLine(x1, y1, x2, y2, context, propsValues);
         if (propsValues.selectedTool.eraser) asEraser(x1, y1, x2, y2, context, propsValues);
+        if (propsValues.selectedTool.curve) asCurve(x1, y1, x2, y2, context, propsValues, curvePointX, curvePointY);
     };
 
 
@@ -97,6 +116,17 @@ function CanvasComp(props) {
         context.lineWidth = propsValues.thickness * 2;
         context.moveTo(x1, y1);
         context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+    }
+
+    function asCurve(x1, y1, x2, y2, context, propsValues, curvePointX, curvePointY) {
+        context.beginPath();
+        context.strokeStyle = propsValues.color;
+        context.lineWidth = propsValues.thickness;
+        context.moveTo(x1, y1);
+        context.quadraticCurveTo(curvePointX, curvePointY, x2, y2);
+        console.log(x1, y1, x2, y2);
         context.stroke();
         context.closePath();
     }
