e65cb8b62b8a8224abfca25c576ae2a2d031ce10
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-01-13 10:29:44 -0300

    Draw circle functionallity on, the only thing that stay for fix heve been the draw with the mouse right to left

--- src/components/CanvasComp/CanvasComp.jsx
+++ src/components/CanvasComp/CanvasComp.jsx
@@ -20,6 +20,9 @@ function CanvasComp(props) {
         var curveX = [];
         var curveY = [];
 
+        var widthCircle = [];
+        var heightCircle = [];
+
         var canvas = document.getElementById('Canvas');
         var context = canvas.getContext('2d');
         var rect = canvas.getBoundingClientRect();
@@ -47,6 +50,10 @@ function CanvasComp(props) {
                     curveX.push(x2);
                     curveY.push(y2);
                 }
+                if (propsValues.selectedShape.circle || propsValues.selectedShape.circleF) {
+                    widthCircle.push(x2);
+                    heightCircle.push(y2);
+                }
             }
         })
 
@@ -55,6 +62,7 @@ function CanvasComp(props) {
 
             x2 = e.clientX - rect.left;
             y2 = e.clientY - rect.top;
+
             
             let itemX = Number((curveX.length / 2).toFixed());
             let itemY = Number((curveY.length / 2).toFixed());
@@ -63,8 +71,14 @@ function CanvasComp(props) {
             let curvePointY = curveY[itemY];
 
 
+            let itemCircleX = Number((widthCircle.length / 2).toFixed());
+            let itemCircleY = Number((heightCircle.length / 2).toFixed());
+
+            let middlePointX = widthCircle[itemCircleX];
+            let middlePointY = heightCircle[itemCircleY];
+
             if (drawing === true) {
-                onDrawing(x, y, x2, y2, context, propsValues, curvePointX, curvePointY);
+                onDrawing(x, y, x2, y2, context, propsValues, curvePointX, curvePointY, middlePointX, middlePointY);
                 x = 0;
                 y = 0;
                 x2 = 0;
@@ -74,6 +88,8 @@ function CanvasComp(props) {
 
             curveX = [];
             curveY = [];
+            widthCircle = [];
+            heightCircle = [];
         })
 
         canvas.addEventListener('mouseout', () => {
@@ -82,7 +98,7 @@ function CanvasComp(props) {
         
     }, [])
 
-    function onDrawing(x1, y1, x2, y2, context, propsValues, curvePointX, curvePointY) {
+    function onDrawing(x1, y1, x2, y2, context, propsValues, curvePointX, curvePointY, middlePointX, middlePointY) {
         console.log(propsValues);
         if (propsValues.selectedTool.pencil) asPencil(x1, y1, x2, y2, context, propsValues);
         if (propsValues.selectedTool.line) asLine(x1, y1, x2, y2, context, propsValues);
@@ -100,11 +116,11 @@ function CanvasComp(props) {
             }
             if (propsValues.selectedShape.circle) {
                 fill = false;
-                asCircle(x1, y1, x2, y2, context, propsValues, fill);
+                asCircle(x1, y1, x2, y2, context, propsValues, fill, middlePointX, middlePointY);
             }
             if (propsValues.selectedShape.circleF) {
                 fill = true;
-                asCircle(x1, y1, x2, y2, context, propsValues, fill);
+                asCircle(x1, y1, x2, y2, context, propsValues, fill, middlePointX, middlePointY);
             }
         }
     };
@@ -167,10 +183,22 @@ function CanvasComp(props) {
     }
 
     function asCircle(x1, y1, x2, y2, context, propsValues, fill) {
+        let centerX = (x2 - x1) / 2;
+        let centerY = (y2 - y1) / 2;
+        if (centerX < 0) return centerX * -1;
+        if (centerY < 0) return centerY * -1;
+        console.log(centerX, centerY);
         context.beginPath();
         context.strokeStyle = propsValues.color;
         context.lineWidth = propsValues.thickness;
-        context.arc(200, 200, 70, 0, 2 * Math.PI, false);
+        if (fill) {
+            context.fillStyle = propsValues.color;
+            context.arc(centerX + x1, centerY + y1, (centerX + centerY) / 2, 0, 2 * Math.PI, false);
+            context.fill();
+        }
+        if (!fill) {
+            context.arc(centerX + x1, centerY + y1, (centerX + centerY) / 2, 0, 2 * Math.PI, false);
+        }
         context.stroke();
         context.closePath();
     }
